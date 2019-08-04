be92d5174fbaccce8f3022533beeaf446948a576
Neyasbltb <neyasbltb_88@mail.ru>
2019-08-04 19:48:07 +0300

    Пример использования XHR в TestSpeachApi

--- src/scripts/modules/TestSpeachApi.js
+++ src/scripts/modules/TestSpeachApi.js
@@ -1,8 +1,21 @@
-import XHR from './xhr';
+import Xhr from './xhr';
 
 export default class TestSpeachApi {
     constructor() {
         console.log('%c%s', (window.log_color) ? window.log_color.blue : '', 'TestSpeachApi');
+
+        this.XHR = new Xhr();
+        this.testMsgUrl = 'https://psv4.userapi.com/c852436//u281986614/audiomsg/d8/fddec1089c.mp3';
+
+        this.test();
+    }
+
+    async test() {
+        let res = await this.XHR.request(this.testMsgUrl, {
+            responseType: 'arraybuffer',
+        });
+
+        console.log(res);
 
     }
 }
