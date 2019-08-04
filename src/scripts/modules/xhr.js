5ca4e1e5c37eca534a8960e0707e123928d926f2
Neyasbltb <neyasbltb_88@mail.ru>
2019-08-04 21:03:34 +0300

    Изменения в Xhr

--- src/scripts/modules/xhr.js
+++ src/scripts/modules/xhr.js
@@ -4,64 +4,50 @@ export default class Xhr {
         this.xhr_counter = 0;
     }
 
-    request(url, params = {}) {
-        let xhr_index = ++this.xhr_counter;
+    request(url, {
+        method = 'GET',
+        responseType,
+        body = '',
+        headers,
+        withCredentials
+    } = {}, progressCallback = () => {}) {
+
+        let xhr_index = this.xhr_counter++;
         this.xhr[xhr_index] = new XMLHttpRequest();
-        // log.dev('xhr_index: ', xhr_index);
+        this.xhr[xhr_index].responseType = responseType || 'text';
+        this.xhr[xhr_index].withCredentials = withCredentials || false;
 
-        this.xhr[xhr_index].withCredentials = params.withCredentials || false;
-        this.xhr[xhr_index].responseType = params.responseType || 'text';
-        this.xhr[xhr_index].timeout = params.timeout || -0;
-
-        let method = params.method || 'GET';
-        let async = params.async === false ? false : true;
-        let data = params.data || '';
+        this.xhr[xhr_index].addEventListener('progress', e => {
+            if (typeof progressCallback === 'function') progressCallback(e);
+        });
 
         return new Promise((resolve, reject) => {
-            this.xhr[xhr_index].open(method, url, async);
-
             this.xhr[xhr_index].addEventListener('load', e => {
-                // Если сервер ответил не 200
-                if (e.target.status != 200) {
-                    delete this.xhr[xhr_index];
-                    reject({
-                        reason: `Status: ${e.target.status}`,
-                        event: e
-                    });
-
-                    return;
-                }
-
-                // Если все хорошо
                 delete this.xhr[xhr_index];
                 resolve(e);
             });
-
-            this.xhr[xhr_index].addEventListener('error', e => {
-                delete this.xhr[xhr_index];
-                reject({
-                    reason: `Error`,
-                    event: e
-                });
-            });
-
             this.xhr[xhr_index].addEventListener('abort', e => {
                 delete this.xhr[xhr_index];
-                reject({
-                    reason: `Abort`,
-                    event: e
-                });
+                reject(e);
             });
-
+            this.xhr[xhr_index].addEventListener('error', e => {
+                delete this.xhr[xhr_index];
+                reject(e);
+            });
             this.xhr[xhr_index].addEventListener('timeout', e => {
                 delete this.xhr[xhr_index];
-                reject({
-                    reason: `Timeout`,
-                    event: e
-                });
+                reject(e);
             });
 
-            this.xhr[xhr_index].send(data);
+            this.xhr[xhr_index].open(method, url, true);
+
+            if (headers) {
+                for (let header in headers) {
+                    this.xhr[xhr_index].setRequestHeader(header, headers[header]);
+                }
+            }
+
+            this.xhr[xhr_index].send(body);
         });
     }
 
