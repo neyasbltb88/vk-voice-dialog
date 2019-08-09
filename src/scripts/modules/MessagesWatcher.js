b9b8dce97e55dc3aa1487818165600c5e4c6e5f0
sasa02sasa <sasa02sasa@mail.ru>
2019-08-09 14:45:49 +0600

    Добавлен внутренний обсервер(просмотр внутрениих сообщений)

--- src/scripts/modules/MessagesWatcher.js
+++ src/scripts/modules/MessagesWatcher.js
@@ -1,30 +1,59 @@
 export default class MessagesWatcher {
+    
+    regexp = /^\d+$/gm;
+    
     constructor() {
         this.messagesId = new Set();
         this.target = null;
-        this.observer = null;
+        this.externalObserver = null;
+        this.externalMessage = null;
+        this.interiorObserver = null;
+        this.interiorMessage = null;
     }
     watcher = (element, callback) => {
         this.target = element;
-        this.observer = new MutationObserver(mutations => {
+        this.externalObserver = new MutationObserver(mutations => {
             mutations.forEach(mutation => {
                 if (mutation.type == 'childList') {
                     if (mutation.addedNodes.length !== 0) {
-                        const regexp = /^\d+$/gm;
                         const lastChild = mutation.addedNodes[0];
-                        const id = regexp.exec(lastChild.querySelector('.im-mess').dataset.msgid);
+                        const id = this.regexp.exec(lastChild.querySelector('.im-mess').dataset.msgid);
                         if (id !== null) {
                             const block = lastChild.querySelector('.im-mess--text');
+                            const listMessages = lastChild.querySelector('.ui_clean_list');
                             if (typeof callback == 'function') {
-                                callback(block)
+                                callback(block);
+                                this.messagesId.add(id);
+                                this.interiorWatcher(listMessages, console.log)
                             }
-                            this.messagesId.add(id);
                         }
                     }
                 }
             });
         })
-        this.observer.observe(this.target, {
+        this.externalObserver.observe(this.target, {
+            childList: true,
+        })
+    }
+    interiorWatcher = (element, callback) => {
+        this.interiorObserver = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.type == 'childList') {
+                    if (mutation.addedNodes.length !== 0) {
+                        const lastChild = mutation.addedNodes[0];
+                        const id = this.regexp.exec(lastChild.dataset.msgid);
+                        if (id !== null) {
+                            const block = lastChild.querySelector('.im-mess--text').textContent;
+                            if (typeof callback == 'function') {
+                                callback(block);
+                            }
+                        }
+                    }
+                }
+            })
+        })
+
+        this.interiorObserver.observe(element, {
             childList: true,
         })
     }
